0f4e4c99d668bb8278aabfffab91f8a41546d92c
Kausik Ray <ncrtsmobile@gmail.com>
2018-09-05 12:34:27 +0530

    Added new view controller (viewcontroller2) and new push button function

---
+++ DemoBaseView/ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  DemoBaseView
+//
+//  Created by Dipanjan on 9/5/18.
+//  Copyright © 2018 Dipanjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func btnPopToHomeAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
